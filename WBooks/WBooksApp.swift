17ec3c857a6740dfe08b37e54dca8c9093ac978a
Cristian Romero <c.romero.acosta@wolox.co>
2022-10-24 06:19:29 -0500

    First commit

---
+++ WBooks/WBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBooksApp.swift
+//  WBooks
+//
+//  Created by Cristian Romero on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
