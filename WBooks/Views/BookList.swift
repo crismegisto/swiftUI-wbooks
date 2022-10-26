8ce028fe700b06da73d57d5b6fd3a845733caa63
Cristian Romero <c.romero.acosta@wolox.co>
2022-10-26 08:21:10 -0500

    navigation bar modification

--- WBooks/Views/BookList.swift
+++ WBooks/Views/BookList.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct BookList: View {
+    init() {
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.backgroundColor = UIColor(Color(hue: 0.558, saturation: 0.735, brightness: 0.949)
+        )
+        coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+        UINavigationBar.appearance().tintColor = .white
+    }
+    
     var body: some View {
         NavigationView {
             List(books) { book in
                 BookCard(book: book)
-                    .listRowBackground(ZStack {
+                    .listRowBackground(
                         Color(red: 0.7647058823529411, green: 0.8745098039215686, blue: 0.9607843137254902)
-                        
-                    })
+                    )
                     .listRowSeparator(.hidden)
             }
             .listStyle(GroupedListStyle())
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitle("LIBRARY")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading) {
+                    IconButton("ic_notifications", action: {
+                        print("iconButton pressed")
+                    })
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    IconButton("ic_search", action: {
+                        print("iconButton pressed")
+                    })
+                }
+            }
         }
     }
 }
